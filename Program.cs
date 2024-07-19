8566650124e9385f58bc8f7874a4ce57a688b8b4
1sancaktar15 <99810559+1sancaktar15@users.noreply.github.com>
2024-07-19 15:42:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms; // Application sınıfını ekledik
+
+namespace StepMotorControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // Form1'i başlat
+        }
+    }
+}
