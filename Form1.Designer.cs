9511804352fe6f929aa52a0430d6639fa2391f6d
1sancaktar15 <99810559+1sancaktar15@users.noreply.github.com>
2024-07-23 10:43:10 +0300

    combobox eklendi Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -12,6 +12,8 @@ namespace StepMotorControl
         private Button btnSagaDon;
         private Button btnSolaDon;
         private Label lblSteps;
+        private ComboBox comboBoxPorts;
+        private Label lblSelectPort;
 
         private void InitializeComponent()
         {
@@ -21,13 +23,15 @@ namespace StepMotorControl
             this.btnSagaDon = new Button();
             this.btnSolaDon = new Button();
             this.lblSteps = new Label();
+            this.comboBoxPorts = new ComboBox();
+            this.lblSelectPort = new Label();
             ((ISupportInitialize)(this.numericUpDownAdim)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDownAdim
             // 
             this.numericUpDownAdim.Name = "numericUpDownAdim";
-            this.numericUpDownAdim.Location = new Point(105, 60);
+            this.numericUpDownAdim.Location = new Point(105, 90);
             this.numericUpDownAdim.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
             this.numericUpDownAdim.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             this.numericUpDownAdim.Size = new Size(180, 40);
@@ -38,7 +42,7 @@ namespace StepMotorControl
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new Point(30, 120);
+            this.btnConnect.Location = new Point(30, 150);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new Size(150, 45);
             this.btnConnect.TabIndex = 1;
@@ -49,7 +53,7 @@ namespace StepMotorControl
             // 
             // btnDisconnect
             // 
-            this.btnDisconnect.Location = new Point(190, 120);
+            this.btnDisconnect.Location = new Point(190, 150);
             this.btnDisconnect.Name = "btnDisconnect";
             this.btnDisconnect.Size = new Size(150, 45);
             this.btnDisconnect.TabIndex = 2;
@@ -60,7 +64,7 @@ namespace StepMotorControl
             // 
             // btnSagaDon
             // 
-            this.btnSagaDon.Location = new Point(30, 180);
+            this.btnSagaDon.Location = new Point(30, 210);
             this.btnSagaDon.Name = "btnSagaDon";
             this.btnSagaDon.Size = new Size(150, 45);
             this.btnSagaDon.TabIndex = 3;
@@ -71,7 +75,7 @@ namespace StepMotorControl
             // 
             // btnSolaDon
             // 
-            this.btnSolaDon.Location = new Point(190, 180);
+            this.btnSolaDon.Location = new Point(190, 210);
             this.btnSolaDon.Name = "btnSolaDon";
             this.btnSolaDon.Size = new Size(150, 45);
             this.btnSolaDon.TabIndex = 4;
@@ -84,7 +88,7 @@ namespace StepMotorControl
             // 
             this.lblSteps.AutoSize = true;
             this.lblSteps.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-            this.lblSteps.Location = new Point(75, 30);
+            this.lblSteps.Location = new Point(75, 60);
             this.lblSteps.Name = "lblSteps";
             this.lblSteps.Size = new Size(240, 27);
             this.lblSteps.TabIndex = 5;
@@ -92,9 +96,33 @@ namespace StepMotorControl
             this.lblSteps.TextAlign = ContentAlignment.MiddleCenter;
             this.lblSteps.Anchor = AnchorStyles.None;
             // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new Point(105, 30);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new Size(180, 21);
+            this.comboBoxPorts.TabIndex = 6;
+            this.comboBoxPorts.Anchor = AnchorStyles.None;
+            // 
+            // lblSelectPort
+            // 
+            this.lblSelectPort.AutoSize = true;
+            this.lblSelectPort.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelectPort.Location = new Point(115, 10);
+            this.lblSelectPort.Name = "lblSelectPort";
+            this.lblSelectPort.Size = new Size(160, 18);
+            this.lblSelectPort.TabIndex = 7;
+            this.lblSelectPort.Text = "Select Serial Port:";
+            this.lblSelectPort.TextAlign = ContentAlignment.MiddleCenter;
+            this.lblSelectPort.Anchor = AnchorStyles.None;
+            // 
             // Form1
             // 
             this.ClientSize = new Size(390, 270);
+            this.Controls.Add(this.lblSelectPort);
+            this.Controls.Add(this.comboBoxPorts);
             this.Controls.Add(this.lblSteps);
             this.Controls.Add(this.btnSolaDon);
             this.Controls.Add(this.btnSagaDon);
@@ -106,6 +134,7 @@ namespace StepMotorControl
             this.FormClosing += new FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
+            ((ISupportInitialize)(this.numericUpDownAdim)).EndInit();
         }
     }
 }
