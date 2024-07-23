f5143c0090b916a7be2697f686297f5f0e1822e6
1sancaktar15 <99810559+1sancaktar15@users.noreply.github.com>
2024-07-23 09:33:02 +0300

    arayüzün son hali Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,121 +2,110 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace StepMotorControl;
-
-partial class Form1
-{  
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private IContainer components = null;
-
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+namespace StepMotorControl
+{
+    partial class Form1
     {
-        if (disposing && (components != null))
+        private NumericUpDown numericUpDownAdim;
+        private Button btnConnect;
+        private Button btnDisconnect;
+        private Button btnSagaDon;
+        private Button btnSolaDon;
+        private Label lblSteps;
+
+        private void InitializeComponent()
         {
-            components.Dispose();
+            this.numericUpDownAdim = new NumericUpDown();
+            this.btnConnect = new Button();
+            this.btnDisconnect = new Button();
+            this.btnSagaDon = new Button();
+            this.btnSolaDon = new Button();
+            this.lblSteps = new Label();
+            ((ISupportInitialize)(this.numericUpDownAdim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownAdim
+            // 
+            this.numericUpDownAdim.Name = "numericUpDownAdim";
+            this.numericUpDownAdim.Location = new Point(105, 60);
+            this.numericUpDownAdim.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            this.numericUpDownAdim.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            this.numericUpDownAdim.Size = new Size(180, 40);
+            this.numericUpDownAdim.Font = new Font(this.numericUpDownAdim.Font.FontFamily, 18);
+            this.numericUpDownAdim.TabIndex = 0;
+            this.numericUpDownAdim.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            this.numericUpDownAdim.Anchor = AnchorStyles.None;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new Point(30, 120);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new Size(150, 45);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new EventHandler(this.btnConnect_Click);
+            this.btnConnect.Anchor = AnchorStyles.None;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new Point(190, 120);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new Size(150, 45);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new EventHandler(this.btnDisconnect_Click);
+            this.btnDisconnect.Anchor = AnchorStyles.None;
+            // 
+            // btnSagaDon
+            // 
+            this.btnSagaDon.Location = new Point(30, 180);
+            this.btnSagaDon.Name = "btnSagaDon";
+            this.btnSagaDon.Size = new Size(150, 45);
+            this.btnSagaDon.TabIndex = 3;
+            this.btnSagaDon.Text = "Turn Right";
+            this.btnSagaDon.UseVisualStyleBackColor = true;
+            this.btnSagaDon.Click += new EventHandler(this.btnSagaDon_Click);
+            this.btnSagaDon.Anchor = AnchorStyles.None;
+            // 
+            // btnSolaDon
+            // 
+            this.btnSolaDon.Location = new Point(190, 180);
+            this.btnSolaDon.Name = "btnSolaDon";
+            this.btnSolaDon.Size = new Size(150, 45);
+            this.btnSolaDon.TabIndex = 4;
+            this.btnSolaDon.Text = "Turn Left";
+            this.btnSolaDon.UseVisualStyleBackColor = true;
+            this.btnSolaDon.Click += new EventHandler(this.btnSolaDon_Click);
+            this.btnSolaDon.Anchor = AnchorStyles.None;
+            // 
+            // lblSteps
+            // 
+            this.lblSteps.AutoSize = true;
+            this.lblSteps.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            this.lblSteps.Location = new Point(75, 30);
+            this.lblSteps.Name = "lblSteps";
+            this.lblSteps.Size = new Size(240, 27);
+            this.lblSteps.TabIndex = 5;
+            this.lblSteps.Text = "Enter number of steps:";
+            this.lblSteps.TextAlign = ContentAlignment.MiddleCenter;
+            this.lblSteps.Anchor = AnchorStyles.None;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new Size(390, 270);
+            this.Controls.Add(this.lblSteps);
+            this.Controls.Add(this.btnSolaDon);
+            this.Controls.Add(this.btnSagaDon);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.numericUpDownAdim);
+            this.Name = "Form1";
+            this.Text = "Step Motor Control";
+            this.FormClosing += new FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-        base.Dispose(disposing);
     }
-
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private Button btnConnect;
-    private Button btnDisconnect;
-    private Button btnSolaDon;
-    private Button btnSagaDon;
-    private TextBox txtAdim;
-
-    private void InitializeComponent()
-    {
-        this.btnConnect = new System.Windows.Forms.Button();
-        this.btnDisconnect = new System.Windows.Forms.Button();
-        this.btnSolaDon = new System.Windows.Forms.Button();
-        this.btnSagaDon = new System.Windows.Forms.Button();
-        this.txtAdim = new System.Windows.Forms.TextBox();
-        
-        // 
-        // btnConnect
-        // 
-        this.btnConnect.Location = new System.Drawing.Point(12, 12);
-        this.btnConnect.Name = "btnConnect";
-        this.btnConnect.Size = new System.Drawing.Size(100, 23);
-        this.btnConnect.TabIndex = 0;
-        this.btnConnect.Text = "Connect";
-        this.btnConnect.UseVisualStyleBackColor = true;
-        this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
-        
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 600);
-        this.Text = "Step Motor Control";
-        
-        // Form ölçülerine göre orantılı boyutlar ve yerleşimler
-        int buttonWidth = 120;
-        int buttonHeight = 40;
-        int spacing = 20;
-        int textBoxWidth = 120;
-        int textBoxHeight = 30;
-
-        // Connect Button
-        Button btnConnect = new Button();
-        btnConnect.Text = "Connect";
-        btnConnect.Size = new Size(buttonWidth, buttonHeight);
-        btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
-
-        // Disconnect Button
-        Button btnDisconnect = new Button();
-        btnDisconnect.Text = "Disconnect";
-        btnDisconnect.Size = new Size(buttonWidth,buttonHeight);
-        btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
-
-        // Sola Dön Button
-        Button btnSolaDon = new Button();
-        btnSolaDon.Text = "Sola Dön";
-        btnSolaDon.Size = new Size(buttonWidth, buttonHeight);
-        btnSolaDon.Click += new System.EventHandler(this.btnSolaDon_Click);
-
-        // Sağa Dön Button
-        Button btnSagaDon = new Button();
-        btnSagaDon.Text = "Sağa Dön";
-        btnSagaDon.Size = new Size(buttonWidth, buttonHeight);
-        btnSagaDon.Click += new System.EventHandler(this.btnSagaDon_Click);
-
-        // Adım TextBox
-        TextBox txtAdim = new TextBox();
-        txtAdim.Size = new Size(textBoxWidth, textBoxHeight);
-
-        // Adding controls to the form
-        this.Controls.Add(btnConnect);
-        this.Controls.Add(btnDisconnect);
-        this.Controls.Add(btnSolaDon);
-        this.Controls.Add(btnSagaDon);
-        this.Controls.Add(txtAdim);
-        
-        int totalWidth = buttonWidth * 2 + spacing;
-        int totalHeight = buttonHeight * 2 + textBoxHeight + spacing * 3;
-        int startX = (this.ClientSize.Width - totalWidth) / 2;
-        int startY = (this.ClientSize.Height - totalHeight) / 2;
-
-        // Set positions
-        btnConnect.Location = new Point(startX, startY);
-        btnDisconnect.Location = new Point(startX + buttonWidth + spacing, startY);
-        btnSolaDon.Location = new Point(startX, startY + buttonHeight + spacing);
-        btnSagaDon.Location = new Point(startX + buttonWidth + spacing, startY + buttonHeight + spacing);
-        txtAdim.Location = new Point((this.ClientSize.Width - textBoxWidth) / 2, startY + buttonHeight * 2 + spacing * 2);
-        
-    }
-
-
-    #endregion
 }
