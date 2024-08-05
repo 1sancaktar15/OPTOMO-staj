22fcfef7ab680c5b2906bc45033a7cb8385fe28e
1sancaktar15 <99810559+1sancaktar15@users.noreply.github.com>
2024-08-05 16:37:49 +0300

    Update stepMotorControl.c

--- stepMotorControl.c
+++ stepMotorControl.c
@@ -18,9 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "string.h"
-#include "stdio.h"
-#include "math.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -69,289 +66,286 @@ static void MX_USART1_UART_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init(); // HAL kütüphanesini başlat
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init(); // initialize GPIO
-  MX_USART1_UART_Init(); // initialize uart1
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART1_UART_Init();
+	/* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
 
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 		/*
-		HAL_GPIO_WritePin(motor_ena_pozitif_GPIO_Port, motor_ena_pozitif_Pin, GPIO_PIN_SET); // motoru enable et
-		for (uint32_t i = 0; i < 6400; i++) {
-			HAL_GPIO_WritePin(motor_pulse_GPIO_Port, motor_pulse_Pin,
-					GPIO_PIN_SET); // motor pulse pinini high yap
-			HAL_Delay(1); // 1 ms bekle
-			HAL_GPIO_WritePin(motor_pulse_GPIO_Port, motor_pulse_Pin,
-					GPIO_PIN_RESET); // motor pulse pinini low yap
-			HAL_Delay(1); // 1 ms bekle
-		}
-		*/
-		if(packetCompleted == 1){
+		 HAL_GPIO_WritePin(motor_ena_pozitif_GPIO_Port, motor_ena_pozitif_Pin, GPIO_PIN_SET); // motoru enable et
+		 for (uint32_t i = 0; i < 6400; i++) {
+		 HAL_GPIO_WritePin(motor_pulse_GPIO_Port, motor_pulse_Pin,
+		 GPIO_PIN_SET); // motor pulse pinini high yap
+		 HAL_Delay(1); // 1 ms bekle
+		 HAL_GPIO_WritePin(motor_pulse_GPIO_Port, motor_pulse_Pin,
+		 GPIO_PIN_RESET); // motor pulse pinini low yap
+		 HAL_Delay(1); // 1 ms bekle
+		 }
+		 */
+		if (packetCompleted == 1) {
+			/*
+			for (uint8_t i = 0; i < 7; i++) {
+				sprintf(data, "\f %d \n\r", dataPacket[i]);
+				HAL_UART_Transmit(&huart1, data, strlen(data), 1000);
+				HAL_Delay(1000);
+			}
+			*/
 			packetCompleted = 0;
 			convertDataNumber();
 			motorStart(dataPacket[1] - 0x30, intStepValue);
-
 		}
 
 		HAL_Delay(2000); // 2 sn. bekle
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
-  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
-  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART1_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
-  HAL_UART_Receive_IT(&huart1, &rData[0], 1); // UART1 için kesmeli veri alma işlemini etkinleştir. Bu, UART üzerinden veri alındığında bir kesme meydana geleceği anlamına gelir ve veri alındığında 'rData' tamponuna yazılacaktır.
-  /* USER CODE END USART1_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOF_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, motor_ena_negatif_Pin|motor_dir_Pin|motor_pulse_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(motor_ena_pozitif_GPIO_Port, motor_ena_pozitif_Pin, GPIO_PIN_SET);
-
-  /*Configure GPIO pins : motor_ena_negatif_Pin motor_ena_pozitif_Pin motor_dir_Pin motor_pulse_Pin */
-  GPIO_InitStruct.Pin = motor_ena_negatif_Pin|motor_ena_pozitif_Pin|motor_dir_Pin|motor_pulse_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-	if(HAL_UART_Receive_IT(&huart1, &rData[0], 1) != HAL_OK){
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
 		Error_Handler();
 	}
 
-	if(check == 0){
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+	PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+/**
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
+
+	/* USER CODE BEGIN USART1_Init 0 */
+
+	/* USER CODE END USART1_Init 0 */
+
+	/* USER CODE BEGIN USART1_Init 1 */
+
+	/* USER CODE END USART1_Init 1 */
+	huart1.Instance = USART1;
+	huart1.Init.BaudRate = 9600;
+	huart1.Init.WordLength = UART_WORDLENGTH_8B;
+	huart1.Init.StopBits = UART_STOPBITS_1;
+	huart1.Init.Parity = UART_PARITY_NONE;
+	huart1.Init.Mode = UART_MODE_TX_RX;
+	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&huart1) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN USART1_Init 2 */
+	HAL_UART_Receive_IT(&huart1, &rData[0], 1); // UART1 için kesmeli veri alma işlemini etkinleştir. Bu, UART üzerinden veri alındığında bir kesme meydana geleceği anlamına gelir ve veri alındığında 'rData' tamponuna yazılacaktır.
+	/* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
+	/* USER CODE END MX_GPIO_Init_1 */
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOF_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOB,
+	motor_ena_negatif_Pin | motor_dir_Pin | motor_pulse_Pin, GPIO_PIN_RESET);
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(motor_ena_pozitif_GPIO_Port, motor_ena_pozitif_Pin,
+			GPIO_PIN_SET);
+
+	/*Configure GPIO pins : motor_ena_negatif_Pin motor_ena_pozitif_Pin motor_dir_Pin motor_pulse_Pin */
+	GPIO_InitStruct.Pin = motor_ena_negatif_Pin | motor_ena_pozitif_Pin
+			| motor_dir_Pin | motor_pulse_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
+	/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	if (HAL_UART_Receive_IT(&huart1, &rData[0], 1) != HAL_OK) {
+		Error_Handler();
+	}
+
+	if (check == 0) {
 		dataPacket[check] = rData[0];
-		if(dataPacket[check] == 'M'){
+		if (dataPacket[check] == 'M') {
 			check = 1;
-		}
-		else{
+		} else {
 			check = 0;
 		}
-	}else if(check == 1 && dataPacket[0] == 'M'){
+	} else if (check == 1 && dataPacket[0] == 'M') {
 		dataPacket[check] = rData[0];
-		if(dataPacket[check] == '1' || dataPacket[check] == '0'){
+		if (dataPacket[check] == '1' || dataPacket[check] == '0') {
 			check = 2;
-		}else{
+		} else {
 			check = 0;
 		}
-	}else if(check == 2 && dataPacket[0] == 'M'){
+	} else if (check == 2 && dataPacket[0] == 'M') {
 		dataPacket[check] = rData[0];
-		if(dataPacket[check] > 0x2F && dataPacket[check] < 0x3A){
+		if (dataPacket[check] > 0x2F && dataPacket[check] < 0x3A) {
 			check = 3;
-		}else{
+		} else {
 			check = 0;
 		}
-	}else if(check == 3 && dataPacket[0] == "M"){
+	} else if (check == 3 && dataPacket[0] == "M") {
 		dataPacket[check] = rData[0];
-		if(dataPacket[check] > 0x2F && dataPacket[check] < 0x3A){
+		if (dataPacket[check] > 0x2F && dataPacket[check] < 0x3A) {
 			check = 4;
-		}else{
+		} else {
 			check = 0;
 		}
-	}else if(check == 4 && dataPacket[0] == 'M'){
+	} else if (check == 4 && dataPacket[0] == 'M') {
 		dataPacket[check] = rData[0];
-		if(dataPacket[check] > 0x2F && dataPacket[check] < 0x3A){
+		if (dataPacket[check] > 0x2F && dataPacket[check] < 0x3A) {
 			check = 5;
-		}else{
+		} else {
 			check = 0;
 		}
-	}else if(check == 5 && dataPacket[0] == 'M'){
+	} else if (check == 5 && dataPacket[0] == 'M') {
 		dataPacket[check] = rData[0];
-		if(dataPacket[check] > 0x2F && dataPacket[check] < 0x3A){
+		if (dataPacket[check] > 0x2F && dataPacket[check] < 0x3A) {
 			check = 6;
-		}else{
+		} else {
 			check = 0;
 		}
-	}else if(check == 6 && dataPacket[0] == 'M'){
+	} else if (check == 6 && dataPacket[0] == 'M') {
 		dataPacket[check] = rData[0];
-		if(dataPacket[check] > 0x2F && dataPacket[check] < 0x3A){
+		if (dataPacket[check] > 0x2F && dataPacket[check] < 0x3A) {
 			check = 0;
 			packetCompleted = 1;
-		}else{
+		} else {
 			check = 0;
 		}
 	}
 }
 
-void convertDataNumber(void){
-	for(uint8_t i = 2; i < 7; i++){
-		dataStepArray[i-2] = dataPacket[i];
+void convertDataNumber(void) {
+	for (uint8_t i = 2; i < 7; i++) {
+		dataStepArray[i - 2] = dataPacket[i];
 	}
 	dataStepArray[5] = '\0';
 	intStepValue = atoi(dataStepArray);
 }
 
-void motorStart(uint8_t direction, uint32_t stepValue){
-	HAL_GPIO_WritePin(motor_ena_pozitif_GPIO_Port, motor_ena_pozitif_Pin, GPIO_PIN_RESET);
+void motorStart(uint8_t direction, uint32_t stepValue) {
+	HAL_GPIO_WritePin(motor_ena_pozitif_GPIO_Port, motor_ena_pozitif_Pin,
+			GPIO_PIN_RESET);
 
-	if(direction){
+	if (direction) {
 		HAL_GPIO_WritePin(motor_dir_GPIO_Port, motor_dir_Pin, GPIO_PIN_SET);
-	}else{
+	} else {
 		HAL_GPIO_WritePin(motor_dir_GPIO_Port, motor_dir_Pin, GPIO_PIN_RESET);
 	}
 
-	for(uint32_t i = 0; i < stepValue; i++){
+	for (uint32_t i = 0; i < stepValue; i++) {
 		HAL_GPIO_WritePin(motor_pulse_GPIO_Port, motor_pulse_Pin, GPIO_PIN_SET);
 		HAL_Delay(1);
-		HAL_GPIO_WritePin(motor_pulse_GPIO_Port, motor_pulse_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(motor_pulse_GPIO_Port, motor_pulse_Pin,
+				GPIO_PIN_RESET);
 		HAL_Delay(1);
 	}
 
-	for(uint8_t i = 0; i < 7; i++){
+	for (uint8_t i = 0; i < 7; i++) {
 		dataPacket[i] = 0;
 	}
 
 	sprintf(data, "MCOM");
-	HAL_UART_Transmit(&huart1, (uint8_t*)data, strlen(data), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart1, (uint8_t*) data, strlen(data), HAL_MAX_DELAY);
 }
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
